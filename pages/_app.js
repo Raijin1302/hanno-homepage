ca0c1c9ba71bd98d34aa20f516d825ca903897c7
Raijin1302 <raijinnguyen1302@gmail.com>
2022-09-08 22:52:39 +0700

    adding progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,19 @@ import Layout from '../components/layouts/main'
 import Fonts from '../components/font'
 import theme from '../lib/theme'
 import { AnimatePresence } from 'framer-motion'
+import ProgressBar from '@badrap/bar-of-progress'
+import Router from 'next/router'
 
+const progress = new ProgressBar({
+  size: 7,
+  color: '#ddec7e',
+  className: 'z-50',
+  delay: 500
+})
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
 const Website = ({ Component, pageProps, router }) => {
   return (
     <ChakraProvider theme={theme}>
